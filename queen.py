f6c14cddd8f3971ead2a37f06a62a76bb7a88524
Pich240 <besreypich8@gmail.com>
2025-10-27 13:05:12 +0700

    initial commit

---
+++ queen.py
@@ -0,0 +1,13 @@
+#str variable
+user_name = "ming"
+#int variable
+user_age = 17
+#float variable
+user_height = 1.60
+#boolean variable
+is_student = True
+
+print("Name:", user_name, "| Type:", type(user_name))
+print("Age:", user_age, " | Type:", type(user_age))
+print("Height:", user_height, "| Type:", type(user_height))
+print("Is Student:", is_student, "| Type:", type(is_student))
