34ed7b37c50d60775b0e3d0169f4e4c9650ae3dd
Pich240 <besreypich8@gmail.com>
2025-10-28 00:07:33 +0700

    using input and f-string

---
+++ best.py
@@ -0,0 +1,16 @@
+print("----------Enter the user info----------")
+# user id is interger 
+user_id = int(input("Enter the user id: "))
+print(f"The user id is: {user_id} of type {type(user_id)}")
+# user name is string
+user_name = str(input("Enter the user name: "))
+print(f"The user name is: {user_name} of type {type(user_name)}")
+# user age
+user_age = int(input("Enter the user age: "))
+print(f"The user age is: {user_age} of type {type(user_age)}")
+# user position
+user_position = str(input("Enter the user position: "))
+print(f"The user position is: {user_position} of type {type(user_position)}")
+# user_id
+user_id = input("enter your user id:")
+print(f"The user id is: {int(user_id)} of type {type(user_id)}")
